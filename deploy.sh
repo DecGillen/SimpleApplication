0bb26bf20f27a661855efffcfd3a613410bc4a26
D Gillen <declangillen@live.com>
2023-07-07 11:16:59 +0100

    added deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install nodejs npm
+
+sudo npm install -g pm2
+
+pm2 stop simple_app
+
+cd SimpleApplication/
+
+npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+
+pm2 start ./bin/www --name simple_app
